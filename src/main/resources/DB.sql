bcb34604b17335ae8dea61149a69498c9d3e0776
A.M. Supun Madhuranga <157113777+smadhuranga@users.noreply.github.com>
2026-02-26 11:25:13 +0530

    DB.sql added

---
+++ src/main/resources/DB.sql
@@ -0,0 +1,16 @@
+Create table User(
+                     Id int auto_increment primary key,
+                     Name varchar(50),
+                     Password varchar(1000)
+);
+
+Create table Student(
+                        Id int auto_increment primary key,
+                        firstName varchar(50),
+                        lastName varchar(50),
+                        email varchar(200),
+                        dateOfBirth date,
+                        enrollmentDate date
+
+
+);
